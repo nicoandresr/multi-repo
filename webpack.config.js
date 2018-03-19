c6bc9f4940602a69fb9a19a4d68f650462f50c9c
nicoandresr <nicoandres.rodriguez@gmail.com>
2018-03-19 15:48:38 -0500

    Indent config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,17 @@
+var cssLoader = {
+  loader: "css-loader",
+  query: { modules: true, localIdentName: '[name]_[local]___[hash:base64:5]' }
+};
+
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+  output: { filename: 'bundle.js' },
+  resolve: { extensions: ['.js', '.jsx'] },
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.scss$/,
-      loaders: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader",
-          query: {
-            modules: true,
-            localIdentName: '[name]_[local]___[hash:base64:5]'
-          }
-        },
-        {
-          loader: "sass-loader"
-        }
-      ]
-    }]
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.scss$/, loaders: ["style-loader", cssLoader, "sass-loader"] }
+    ]
   }
 };
 
