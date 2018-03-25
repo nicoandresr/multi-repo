eda7218b32a380445acee338048ae71f1bfc16cc
nicoandresr <nicoandres.rodriguez@gmail.com>
2018-03-25 16:12:07 -0500

    aspect ratio test

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import AspectRatio from 'js-aspect-ratio';
 import styles from './Main.scss';
 
 class Main extends Component {
   render() {
+    const aspectRatio = new AspectRatio(4, 3);
     return (
       <div className={styles.mainWrapper}>
         <div className={styles.content}>
           webpack + babel + react
+          {aspectRatio.getFullWidth()}
+          {aspectRatio.getFullHeight()}
+          {window.innerHeight}
+          {window.innerWidth}
         </div>
         <a href="buble">buble</a>
       </div>
