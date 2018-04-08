239195b0b7d6ad1a64f65bb04f173806053014a9
nicoandresr <nicoandres.rodriguez@gmail.com>
2018-04-07 22:26:46 -0500

    test app run

---
+++ apprun/src/main.tsx
@@ -0,0 +1,19 @@
+import app from 'apprun';
+
+const state = 0;
+
+const view = state => (
+  <div>
+    <h1>${state}</h1>
+    <button onclick="app.run('-1')">-1</button>
+    <button onclick="app.run('+1')">+1</button>
+  </div>
+);
+
+const update = {
+  '+1': state => state + 1,
+  '-1': state => state - 1
+};
+
+app.start('my-app', state, view, update);
+
